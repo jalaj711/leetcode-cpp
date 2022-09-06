2b11babbc99139152d32d324cdca28119a69357e
Jalaj Kumar <96870071+jalaj711@users.noreply.github.com>
2022-09-06 16:56:06 +0530

    create template for fast starting the problems
    Signed-off-by: Jalaj Kumar <96870071+jalaj711@users.noreply.github.com>

---
+++ template.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+void solve(){
+
+   cout << endl;
+}
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        solve();
+    }
+}
