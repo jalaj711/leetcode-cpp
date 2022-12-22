49c02337181f86fbd3e21ccef2d23f8ac2967c76
Jalaj Kumar <96870071+jalaj711@users.noreply.github.com>
2022-12-22 20:07:36 +0530

    added reverse-string.cpp
    Signed-off-by: Jalaj Kumar <96870071+jalaj711@users.noreply.github.com>

---
+++ leetcode/reverse-string.cpp
@@ -0,0 +1,15 @@
+#include <vector>
+using namespace std;
+
+class Solution
+{
+public:
+    void reverseString(vector<char> &s)
+    {
+        int sz = s.size();
+        for (int i = 0; i < sz / 2; i++)
+        {
+            swap(s[i], s[sz - i - 1]);
+        }
+    }
+};
