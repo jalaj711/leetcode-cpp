9eb733c9a5af5cae23b821ad2508e1b459277dfe
Jalaj Kumar <96870071+jalaj711@users.noreply.github.com>
2023-01-05 16:13:24 +0530

    add APPLEORANGE.cpp
    Signed-off-by: Jalaj Kumar <96870071+jalaj711@users.noreply.github.com>

---
+++ codechef/START72/APPLEORANGE.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+void solve()
+{
+    int n, m;
+    cin >> n >> m;
+    cout << gcd(n, m) << endl;
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+}
