d0c5c34de598487a677de793db2800a89d75ef72
Jalaj Kumar <96870071+jalaj711@users.noreply.github.com>
2023-05-13 10:44:41 +0530

    edited remove-nth-node-from-end-of-linked-list.cpp
    this improves the algorithm and makes for better efficiency
    
    Signed-off-by: Jalaj Kumar <96870071+jalaj711@users.noreply.github.com>

--- leetcode/remove-nth-node-from-end-of-list.cpp
+++ leetcode/remove-nth-node-from-end-of-list.cpp
@@ -1,4 +1,5 @@
 #include <vector>
+#include <iostream>
 using namespace std;
 
 struct ListNode
@@ -15,34 +16,49 @@ class Solution
 public:
     ListNode *removeNthFromEnd(ListNode *head, int n)
     {
-        vector<ListNode *> nodes;
-        ListNode *nextNode = head;
-        while (nextNode != nullptr)
-        {
-            nodes.push_back(nextNode);
-            nextNode = nextNode->next;
+        ListNode *fast = new ListNode(0, head);
+        ListNode *slow = new ListNode(0, head);
+        for(int i=0;i<n;i++) {
+            if (fast -> next != nullptr) {
+                fast = fast -> next;
+            } else {
+                break;
+            }
         }
-        if (nodes.size() > n)
-        {
-            ListNode *next = nodes[nodes.size() - n]->next;
-            nodes[nodes.size() - n - 1]->next = next;
+        while (fast -> next != nullptr) {
+            fast = fast -> next;
+            slow = slow -> next;
         }
-        else
-        {
-            head = head -> next;
-        };
+        if(slow == nullptr || slow -> next == nullptr) {
+            return nullptr;
+        }
+        slow -> next = (slow -> next) -> next;
+        if (slow -> next == head -> next && slow != head) {
+            return slow -> next;
+        }
         return head;
     }
 };
 
+void traverse(ListNode *l)
+{
+	while (l != NULL)
+	{
+		cout << l->val;
+		l = l->next;
+	}
+}
+
 int main()
 {
-    // ListNode *a7 = new ListNode(9);
-    // ListNode *a6 = new ListNode(8, a7);
-    // ListNode *a5 = new ListNode(7, a6);
-    // ListNode *a4 = new ListNode(6, a5);
-    // ListNode *a3 = new ListNode(5, a4);
-    // ListNode *a2 = new ListNode(4, a3);
-    ListNode *a1 = new ListNode(3, nullptr);
-    (new Solution())->removeNthFromEnd(a1, 1);
+    ListNode *a7 = new ListNode(9);
+    ListNode *a6 = new ListNode(8, a7);
+    ListNode *a5 = new ListNode(7, a6);
+    ListNode *a4 = new ListNode(6, a5);
+    ListNode *a3 = new ListNode(5, a4);
+    ListNode *a2 = new ListNode(4, a3);
+    ListNode *a1 = new ListNode(3, a2);
+    ListNode *ans = (new Solution())->removeNthFromEnd(a1, 2);
+    traverse(ans);
+    cout << endl;
 }
