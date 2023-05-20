8c04b99e059ae35ef89c41ca436528eddeae7a6c
Jalaj Kumar <96870071+jalaj711@users.noreply.github.com>
2023-05-20 10:45:06 +0530

    added binary-tree-postorder-traversal.cpp
    Signed-off-by: Jalaj Kumar <96870071+jalaj711@users.noreply.github.com>

---
+++ leetcode/binary-tree-preorder-traversal.cpp
@@ -0,0 +1,37 @@
+#include<vector>
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+         vector<int> ans;
+        if (root == nullptr) return ans;
+        vector<int> left = preorderTraversal(root -> left);
+        vector<int> right = preorderTraversal(root -> right);
+        ans.push_back(root->val);
+        ans.insert(ans.end(), left.begin(), left.end());
+        ans.insert(ans.end(), right.begin(), right.end());
+        return ans;
+    }
+};
+
+int main() {
+    Solution s;
+    TreeNode left1(6);
+    TreeNode right2(5);
+    TreeNode right1(4, nullptr, &right2);
+    TreeNode root(0, &left1, &right1);
+
+    s.preorderTraversal(&root);
+}
