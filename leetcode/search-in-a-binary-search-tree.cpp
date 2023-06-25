5c078b481a02827b1cd6827cdb5049f5f3ba92f8
Jalaj Kumar <96870071+jalaj711@users.noreply.github.com>
2023-06-25 20:17:54 +0530

    added search-in-a-binary-search-tree.cpp
    Signed-off-by: Jalaj Kumar <96870071+jalaj711@users.noreply.github.com>

---
+++ leetcode/search-in-a-binary-search-tree.cpp
@@ -0,0 +1,27 @@
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+public:
+    TreeNode *searchBST(TreeNode *root, int val)
+    {
+        while (root != nullptr)
+        {
+            if (root->val == val)
+                break;
+            else if (val > root->val)
+                root = root->right;
+            else
+                root = root->left;
+        }
+        return root;
+    }
+};
