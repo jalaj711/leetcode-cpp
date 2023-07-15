2595c83a458884fa49b0bd7d4dad93693b7273bc
Jalaj Kumar <96870071+jalaj711@users.noreply.github.com>
2023-07-15 12:08:56 +0530

    added graaph dfs
    Signed-off-by: Jalaj Kumar <96870071+jalaj711@users.noreply.github.com>

--- dataStructures/graph_dfs.cpp
+++ dataStructures/graph_dfs.cpp
@@ -6,16 +6,22 @@ class Solution {
     private:
     void dfs(int start, vector<int>adj[], int vis[], vector<int> &ans) {
         vis[start] = 1;
-        ans.push_back()
+        ans.push_back(start);
+        for(auto it: adj[start]) {
+            if (!vis[it]) {
+                dfs(it, adj, vis, ans);
+            }
+        }
     }
   public:
     // Function to return Breadth First Traversal of given graph.
     vector<int> dfsOfGraph(int V, vector<int> adj[]) {
         // Code here
         int vis[V] = {0};
-        queue<int> q;
+        vector<int> ans;
         
-        if (V==0) return dfs;
-        return bfs;
+        if (V==0) return ans;
+        dfs(0, adj, vis, ans);
+        return ans;
     }
 };
