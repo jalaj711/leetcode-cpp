028d94dbd98a9f9dda223b891406e85e91cb92b7
Jalaj <96870071+jalaj711@users.noreply.github.com>
2024-04-08 11:13:59 +0530

    added 0802-find-eventual-safe-states.cpp
    Signed-off-by: Jalaj <96870071+jalaj711@users.noreply.github.com>

--- leetcode/0802-find-eventual-safe-states.cpp
+++ leetcode/0802-find-eventual-safe-states.cpp
@@ -1,4 +1,5 @@
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 enum STATES
@@ -7,58 +8,86 @@ enum STATES
     IN_PROCESS,
     FINISHED
 };
+//
+//class Solution
+//{
+//private:
+//    void isSafeNode(int node, vector<vector<int>> &graph, vector<STATES> &vis, vector<bool> &safe)
+//    {
+//        vis[node] = STATES::IN_PROCESS;
+//        for (auto it=graph[node].begin();it<graph[node].end();it++)
+//        {
+//            if (vis[*it] == STATES::IN_PROCESS)
+//            {
+//
+//                safe[node] = false;
+//                break;
+//            }
+//            else if (vis[*it] == STATES::FINISHED)
+//            {
+//                if (!safe[*it])
+//                {
+//                    safe[node] = false;
+//                    break;
+//                }
+//            }
+//            else
+//            {
+//                isSafeNode(*it, graph, vis, safe);
+//                it--;
+//            }
+//        }
+//        vis[node] = STATES::FINISHED;
+//    }
+//
+//public:
+//    vector<int> eventualSafeNodes(vector<vector<int>> &graph)
+//    {
+//        int n = graph.size();
+//        vector<STATES> vis(n, STATES::UNVISITED);
+//        vector<bool> safe(n, true);
+//
+//        for (int i = 0; i < n; i++)
+//        {
+//            if (!vis[i])
+//                isSafeNode(i, graph, vis, safe);
+//        }
+//
+//        vector<int> ans;
+//        for (int j = 0; j < n; j++)
+//        {
+//            if (safe[j])
+//                ans.push_back(j);
+//        }
+//
+//        return ans;
+//    }
+//};
 
-class Solution
-{
+class Solution {
 private:
-    void isSafeNode(int node, vector<vector<int>> &graph, vector<STATES> &vis, vector<bool> &safe)
-    {
-        vis[node] = STATES::IN_PROCESS;
-        for (auto it=graph[node].begin();it<graph[node].end();it++)
-        {
-            if (vis[*it] == STATES::IN_PROCESS)
-            {
-
-                safe[node] = false;
-                break;
-            }
-            else if (vis[*it] == STATES::FINISHED)
-            {
-                if (!safe[*it])
-                {
-                    safe[node] = false;
-                    break;
-                }
-            }
-            else
-            {
-                isSafeNode(*it, graph, vis, safe);
-                it--;
-            }
+    bool dfs(int i, vector<vector<int>>&graph, vector<bool>&vis,vector<bool>&pathVis, vector<int>&ans) {
+        if (pathVis[i]) return false;
+        if (vis[i]) return true;
+        pathVis[i] = true;
+        vis[i] = true;
+        for (int j=0;j<graph[i].size();j++) {
+            if ((!vis[graph[i][j]]&&!dfs(graph[i][j], graph, vis, pathVis, ans))||pathVis[graph[i][j]]) return false;
         }
-        vis[node] = STATES::FINISHED;
+        ans.push_back(i);
+        pathVis[i] = false;
+        return true;
     }
-
 public:
-    vector<int> eventualSafeNodes(vector<vector<int>> &graph)
-    {
+    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
         int n = graph.size();
-        vector<STATES> vis(n, STATES::UNVISITED);
-        vector<bool> safe(n, true);
-
-        for (int i = 0; i < n; i++)
-        {
-            if (!vis[i])
-                isSafeNode(i, graph, vis, safe);
-        }
-
+        vector<bool> vis(n, false);
+        vector<bool> pathVis(n, false);
         vector<int> ans;
-        for (int j = 0; j < n; j++)
-        {
-            if (safe[j])
-                ans.push_back(j);
+        for (int i=0;i<n;i++) {
+            if (!vis[i]) dfs(i, graph, vis, pathVis, ans);
         }
-
+        sort(ans.begin(), ans.end());
         return ans;
     }
 };
