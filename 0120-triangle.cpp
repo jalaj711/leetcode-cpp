6eb5f78de91c25eda0c641b8ea198ea2505bfcae
Jalaj <96870071+jalaj711@users.noreply.github.com>
2024-08-05 12:14:40 +0530

    added 0120-triangle
    Signed-off-by: Jalaj <96870071+jalaj711@users.noreply.github.com>

---
+++ 0120-triangle.cpp
@@ -0,0 +1,18 @@
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    int minimumTotal(vector<vector<int>>& triangle) {
+        int n = triangle.size();
+        vector<int> dp(n, 0);
+
+        for (int i=0;i<n;i++) dp[i] = triangle[n-1][i];
+
+        for (int i=n-2;i>=0;i--)
+            for (int j=0;j<=i;j++)
+                dp[j] = triangle[i][j] + min(dp[j], dp[j+1]);
+
+        return dp[0];
+    }
+};
