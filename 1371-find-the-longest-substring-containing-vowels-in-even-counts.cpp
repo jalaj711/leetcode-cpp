73aaacde4c9984c74e04174f0d0acd2ed767c5cd
Jalaj <96870071+jalaj711@users.noreply.github.com>
2024-09-15 10:31:00 +0530

    optimsed 1371-find-the-longest-substring-containing-vowels-in-even-counts
    Signed-off-by: Jalaj <96870071+jalaj711@users.noreply.github.com>

--- 1371-find-the-longest-substring-containing-vowels-in-even-counts.cpp
+++ 1371-find-the-longest-substring-containing-vowels-in-even-counts.cpp
@@ -1,43 +1,42 @@
-#include<unordered_map>
+#include<vector>
 #include<string>
 using namespace std;
 
 class Solution {
 public:
     int charToNum(char c) {
-        switch(c) {
+        switch (c) {
             case 'a':
-                return 0;
-            case 'e':
                 return 1;
-            case 'i':
+            case 'e':
                 return 2;
-            case 'o':
-                return 3;
-            case 'u':
+            case 'i':
                 return 4;
+            case 'o':
+                return 8;
+            case 'u':
+                return 16;
             default:
                 return -1;
         }
     }
     int findTheLongestSubstring(string s) {
         int n = s.size();
-        unordered_map<int, int> mp;
+        vector<int> bitmp(32, -1);
 
         int mask = 0;
         int ans = 0;
-        for (int i=0;i<n;i++) {
+        for (int i = 0; i < n; i++) {
             int id = charToNum(s[i]);
-            mask = mask ^ (id == -1 ? 0 : (1 << id));
-            if (mask == 0) ans = i+1;
-            else if (mp.find(mask) != mp.end()) {
-                ans = max(ans, i-mp[mask]+(charToNum(s[mp[mask]])==-1));
-            } else {
-                mp[mask] = i;
-            }
+            if (id != -1)
+                mask = mask ^ id;
+
+            if (bitmp[mask] == -1 && mask != 0)
+                bitmp[mask] = i;
+
+            ans = max(ans, i - bitmp[mask]);
         }
 
         return ans;
-
     }
 };
