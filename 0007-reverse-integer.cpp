4228eb91451e7752d7954ef2327fda8c4422c29d
Jalaj <96870071+jalaj711@users.noreply.github.com>
2024-10-06 13:51:11 +0530

    updated  0007-reverse-integer
    Signed-off-by: Jalaj <96870071+jalaj711@users.noreply.github.com>

--- 0007-reverse-integer.cpp
+++ 0007-reverse-integer.cpp
@@ -1,65 +1,30 @@
-#include <stdlib.h>
-class Solution1
-{
-public:
-    int reverse(int x)
-    {
-        int *ans = (int *)malloc(sizeof(int));
-        *ans = 0;
-        bool isNegative = false;
-        if (x <= -2147483647)
-            return 0;
-        if (x < 0)
-        {
-            isNegative = true;
-            x = -x;
-        }
-        while (x > 0)
-        {
-            if (*ans < 0)
-                return 0;
-            *ans = (*ans) * 10;
-            *ans = (*ans) + x % 10;
-            x /= 10;
-        }
-        if (isNegative)
-            *ans = -*ans;
-        return *ans;
-    }
-};
+#include<cmath>
+#include<climits>
+using namespace std;
 
-class Solution
-{
+class Solution {
 public:
-    int reverse(int x)
-    {
-        int ans = 0;
-        bool isNegative = false;
-        if (x <= -2147483647)
-            return 0;
-        if (x < 0)
-        {
-            isNegative = true;
-            x = -x;
-        }
-        while (x > 0)
-        {
-            if (ans >= 214748365 || (ans >= 214748364 && x % 10 > 7))
-                return 0;
-            ans *= 10;
-            ans += x % 10;
-            x /= 10;
-        }
-        if (isNegative)
-            ans = -ans;
-        return ans;
-    }
-};
+  int reverse(int x) {
+    if (x==0||x==INT_MIN) return 0;
 
-int main()
-{
-    (new Solution())->reverse(1534236469);
-    (new Solution())->reverse(1463847412);
-    (new Solution())->reverse(-123);
-    (new Solution())->reverse(504781);
-}
+    bool neg = x < 0;
+    x = abs(x);
+
+    int digit;
+    int reverse = 0;
+    int temp;
+    int count = log10(x);
+
+    if (count>9 || count==9&&((x%10)>2)) return 0;
+
+    while(x!=0) {
+      digit  = x%10;
+      x = x/10;
+      temp= digit * pow(10, count--);
+      if (INT_MAX-reverse < temp) return 0;
+      reverse += temp;
+    }
+
+    return reverse * (neg?-1:1);
+  }
+};
