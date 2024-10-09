220fcec9184b7c0d84e518a0eabbb33d4cb8d0db
Jalaj <96870071+jalaj711@users.noreply.github.com>
2024-10-09 15:33:20 +0530

    added 1123-lowest-common-ancestor-of-deepest-leaves
    Signed-off-by: Jalaj <96870071+jalaj711@users.noreply.github.com>

---
+++ 1123-lowest-common-ancestor-of-deepest-leaves.cpp
@@ -0,0 +1,37 @@
+#include <algorithm>
+
+using namespace std;
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+
+  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+  TreeNode * ans;
+  int maxDepth;
+  int dfs(TreeNode * root, int d) {
+    int l = d;
+    if (root->left) l= dfs(root->left, d+1);
+    int r = d;
+    if (root->right) r=dfs(root->right, d+1);
+
+    if ((l==r && l>=maxDepth) || (l == d && r > maxDepth) || (r == d && l > maxDepth)) ans = root;
+    maxDepth = max(maxDepth, max(l,r));
+    return max(l,r);
+  }
+  TreeNode* lcaDeepestLeaves(TreeNode* root) {
+    maxDepth = 0;
+    ans = root;
+    dfs(root, 0);
+    return ans;
+  }
+};
